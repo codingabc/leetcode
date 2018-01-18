0203e32ffeb7e6d4cdf2b956b5aac9426214172e
codingabc <codingabc@163.com>
2018-01-18 17:59:41 +0800

    update

--- reverse-integer.py
+++ reverse-integer.py
@@ -4,28 +4,19 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        s = str(x)
-        i = 0
-        j = len(s)-1
-        sign = 1
-        if s[0] == '-':
-        	sign = -1
-        	i = 1
-
-        while j>=i:
-        	if s[j] != '0':
-        		break
-        	j = j - 1
-
         n = 0
-        while j>=i:
-        	n = n + int(s[j]) * (10**(j-i))
-        	j = j - 1
+        s = 1
+        if x < 0:
+        	x = -x
+        	s = -1
+        while x!=0:
+        	n = n*10+x%10
+        	x = x/10
 
-        n = sign* n
-        if n >= -0x7fffffff and n <= 0x80000000:
-        	return n
-        return 0
+        n = n * s
+        if n < -0x7fffffff or n > 0x80000000:
+        	return 0
+        return n
 
 
 
